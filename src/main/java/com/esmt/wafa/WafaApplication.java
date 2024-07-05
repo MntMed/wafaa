0b3843cfb94decae502d5df8887602e7ab013624
fatis <fatismedloutt@gmail.com>
2024-07-05 19:06:02 +0000

    my commit

---
+++ src/main/java/com/esmt/wafa/WafaApplication.java
@@ -0,0 +1,13 @@
+package com.esmt.wafa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esmt/wafa/WafaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmt.wafa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WafaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
